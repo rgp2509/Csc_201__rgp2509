2b42221b02989e4f2b75be541f7b1651847b91cb
RyanP <rgp2509@email.vccs.edu>
2017-09-01 14:41:27 -0400

    Test example.

---
+++ src/Day2Notes.java
@@ -0,0 +1,13 @@
+
+/**
+ * purpose demonstrate how loops iterations work in java.
+ * 1. for
+ * 2. While
+ * 3. do.. while
+ *
+ * License example (insert template here)
+ */
+public class Day2Notes{
+    // loopDemo example
+
+}
