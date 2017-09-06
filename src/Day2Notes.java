28e86fa8822cea2b57e56aab824389beabfcd382
RyanP <rgp2509@email.vccs.edu>
2017-09-06 15:11:47 -0400

    Test example.

--- src/Day2Notes.java
+++ src/Day2Notes.java
@@ -9,5 +9,43 @@
  */
 public class Day2Notes{
     // loopDemo example
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+        System.out.println(oddNumbers(1, 100));
+        // print odd numbers
+        // for loop
+        /*
+        1. initial value for the starting point ie. i=1
+        2. condition when to stop ie. i<100
+        3. increment the initial value ie. i++ or i =i+2
+         */
+       // for(int i=1; i<=100; i=i+2){
+       //     System.out.println(i);
+        }
+
+
+    /**
+     * Returns odd numbers between start and stop
+     * @param start first number of the odd number to display
+     * @param stop  last number of the odd number to display
+     * @return the odd numbers between first number (start) and last number (stop).
+     */
+    public static String oddNumbers(int start, int stop) {
+        // print odd numbers
+        // for loop
+        /*
+        1. initial value for the starting point ie. i=1
+        2. condition when to stop ie. i<100
+        3. increment the initial value ie. i++ or i =i+2
+         */
+        String result="";
+
+        for(int i=start; i<=stop; i=i+2){
+            result =+ i +" ";
+        }
+        return result;
+        // update test (1)
+    }
 
 }
